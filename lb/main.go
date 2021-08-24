a42ebbc241a58866930f91601f63e0fe7085a5c9
Markus Kauppila <markus.kauppila@gmail.com>
2021-08-24 20:37:13 +0300

    Parse simple configuration file

--- lb/main.go
+++ lb/main.go
@@ -1,6 +1,7 @@
 package main
 
-// hello world golang http server, returning the process id
+// read: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/x-forwarded-headers.html
+// read: https://docs.nginx.com/nginx/admin-guide/load-balancer/http-load-balancer/
 
 // load balance a get request in round robin
 //  -- add support for round robin (which one is the next?, counter? a circular list?)
@@ -15,10 +16,12 @@ package main
 // - this could support HTTPS and break it before sending the request as plain HTTP to the target
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 const servers = "http://localhost:4001"
@@ -37,11 +40,11 @@ func forwardRequest(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err.Error())
 		log.Fatal("Error with request")
 	}
+	defer response.Body.Close()
+
 	body, _ := ioutil.ReadAll(response.Body)
 	fmt.Printf("Got response back \nres: %s\n", body)
 	w.Write(body)
-
-	defer response.Body.Close()
 }
 
 func start() {
@@ -50,7 +53,44 @@ func start() {
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }
 
+type Server struct {
+	url string
+}
+type Configuration struct {
+	servers []Server
+}
+
+func ParseConfiguration() (Configuration, error) {
+	data, err := ioutil.ReadFile("../lb.conf")
+	if err != nil {
+		return Configuration{}, errors.New("No configuration file exists.")
+	}
+
+	var servers []Server
+	for _, line := range strings.Split(string(data), "\n") {
+		if len(line) == 0 {
+			break
+		}
+		d := strings.Split(line, " ")
+		url := d[1]
+		server := Server{url: url}
+
+		servers = append(servers, server)
+	}
+
+	return Configuration{servers: servers}, nil
+}
+
+var configuration Configuration
+
 func main() {
+	conf, err := ParseConfiguration()
+	if err != nil {
+		log.Fatalln("Failed to parse configuration...", err)
+	}
+	fmt.Println(conf)
+	configuration = conf
+
 	fmt.Println("Starting up!")
 	start()
 }
