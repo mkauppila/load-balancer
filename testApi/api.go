8055d5803d9f96137a96ffa5d1aef8e1dc357d03
Markus Kauppila <markus.kauppila@gmail.com>
2021-09-04 23:56:02 +0300

    Simplify running test apis

--- testApi/api.go
+++ testApi/api.go
@@ -30,6 +30,6 @@ func main() {
 		panic("no PORT env defined")
 	}
 	fmt.Printf("Running on port: %s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), nil))
 	fmt.Println("Shutting down the test API")
 }
